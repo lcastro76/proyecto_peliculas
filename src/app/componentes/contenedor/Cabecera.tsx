4223ef4205d32a006038a4dafbe15f538c5f1113
lcastro76 <lcastro76@cuc.edu.co>
2024-10-28 09:53:18 -0500

    terminamos hasta inicio

--- src/app/componentes/contenedor/Cabecera.tsx
+++ src/app/componentes/contenedor/Cabecera.tsx
@@ -1,8 +1,83 @@
+import { NavLink } from "react-router-dom";
+import milogo from "../../../assets/img/logoReact.png"
+
 export const Cabecera = () => {
   return (
     <>
-      Hola soy la Cabecera <br />
-      El menú de arriba
+      <nav
+        className="navbar navbar-expand-lg bg-dark border-bottom border-body"
+        data-bs-theme="dark"
+      >
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/">
+            <img src={milogo} alt="El logo" />
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarColor01"
+            aria-controls="navbarColor01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarColor01">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink className="nav-link active" aria-current="page" to="/">
+                  Inicio
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/sede">
+                  Sedes
+                </NavLink>
+              </li>
+
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Peliculas
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <NavLink className="dropdown-item" to="/pelicre">
+                      Registrar Peliculas
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink className="dropdown-item" to="/pelilis">
+                      Listar Peliculas
+                    </NavLink>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <NavLink className="dropdown-item" to="/peliadm">
+                      Administrar Peliculas
+                    </NavLink>
+                  </li>
+                </ul>
+              </li>
+
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/acer">
+                  Acerca De
+                </NavLink>
+              </li>
+            </ul>
+
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
--- src/app/utilidades/rutas/Ruteo.tsx
+++ src/app/utilidades/rutas/Ruteo.tsx
@@ -1,10 +1,26 @@
 import { Route, Routes } from "react-router-dom";
 import { Inicio } from "../../componentes/contenedor/Inicio";
+import { PeliCrear } from "../../componentes/peliculas/PeliCrear";
+import { PeliListar } from "../../componentes/peliculas/PeliListar";
+import { PeliAdmin } from "../../componentes/peliculas/PeliAdmin";
+import { PeliActualiar } from "../../componentes/peliculas/PeliActualizar";
+import { Acerca } from "../../componentes/otros/Acerca";
+import { NoEncontrado } from "../../componentes/contenedor/NoEncontrado";
 
 export const Ruteo = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Inicio/>}/>
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route path="/" element={<Inicio />} />
+
+      <Route path="/pelicre" element={<PeliCrear />} />
+      <Route path="/pelilis" element={<PeliListar />} />
+      <Route path="/peliadm" element={<PeliAdmin />} />
+
+      <Route path="/peliact/:codigo" element={<PeliActualiar />} />
+
+      <Route path="/acer" element={<Acerca />} />
+
+      <Route path="*" element={<NoEncontrado />} />
+    </Routes>
+  );
+};
